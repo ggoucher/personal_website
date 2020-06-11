bdc12a6b3b5f72839827a3dddde84f25f631254f
Gerard Goucher <ggoucher@bowdoin.edu>
2020-06-11 12:06:46 -0400

    Make JS responsive to smartphone screens

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 
 /* Hi! here, we're going to get some nav elements going on... */
 
+var mq = true;
+
 function openNav() {
+    mq = window.matchMedia("(min-width: 500px)").matches;
+
     document.querySelector("nav").style.width = "30vh";
-    document.getElementById("main").style.marginLeft = "30vh";
+    if (mq) {
+        document.getElementById("main").style.marginLeft = "30vh";
+    }
 }
 
 function closeNav() {
     document.querySelector("nav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+    if (mq) {
+        document.getElementById("main").style.marginLeft = "0";
+    }
 }
